bfd7cc13243a0d32e667c2b77a3f8344923f91a7
arielmerin <arielmerino@ciencias.unam.mx>
2020-02-11 14:30:35 -0600

    Tiene las operaciones unitarias de los conjuntos

--- Conjunto/src/Conjunto.java
+++ Conjunto/src/Conjunto.java
@@ -1,11 +1,39 @@
+import java.util.Iterator;
+
 /**
  * Esta clase permite el correcto manejo de los objetos de tipo conjunto, presenta las opraciones basicas como TAD y tambien
  * plantea operaciones entre dos conjuntos
  * @author Ariel Merino Peña 317031326
  * @version 1
  */
-public class Conjunto implements Conjuntable{
+public class Conjunto implements Conjuntable, Iterable{
+
+    public Iterator iterator(){
+        return new Iterador();
+    }
+
+    private class Iterador implements Iterator{
+        private int posicion;
+        
+        public Iterador(){
+            posicion = 0;
+        }
+
+        public boolean hasNext(){
+            return posicion < cardinal;
+        }
+
+        public Object next(){
+            return items[posicion++];
+        }
+
+        public void remove(){
+
+        }
+
+    }
     private Object[] items;
+    private int cardinal;
 
     /**
      * Constructor por omision
@@ -20,6 +48,7 @@ public class Conjunto implements Conjuntable{
      */
     public Conjunto(int size){
         items = new Object[size <= 0 ? 20 : size];
+        cardinal = 0;
         for (int i = 0; i < size; i++) {
             items[i] = null;
         }
@@ -38,36 +67,56 @@ public class Conjunto implements Conjuntable{
 
     @Override
     public void addItem(Object item) {
-
+        if(!contains(item)){
+            items[cardinal] = item;
+        }
     }
 
     @Override
     public void eraseItem(Object item) {
-
+        if (!isEmpty()){
+            for (int i = 0; i < cardinal; i++) {
+                if (item.equals(items[i])){
+                    items[i] = null;
+                    return;
+                }
+            }
+        }
     }
 
     @Override
     public boolean contains(Object item) {
+        if (!isEmpty()){
+            for (int i = 0; i < cardinal ; i++) {
+                if (items[i].equals(item)) return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        for (int i = 0; i < cardinal ; i++) {
+            if(items[i] != null) return false;
+        }
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        return cardinal;
     }
 
     @Override
     public void throwAll() {
-
+        for (int i = 0; i < cardinal ; i++) {
+            items[i] = null;
+        }
     }
 
     @Override
     public Conjuntable union(Conjuntable conjunto1) {
+
         return null;
     }
 
@@ -83,6 +132,13 @@ public class Conjunto implements Conjuntable{
 
     @Override
     public boolean subSet(Conjuntable conjunto) {
+        if (conjunto.size() <= cardinal){
+            for (int i = 0; i < conjunto.size() ; i++) {
+                if (contains(items[i])){
+
+                }
+            }
+        }
         return false;
     }
 }

2fc0e123380f9b99d14fedc8db86db63c62399f1
arielmerin <arielmerino@ciencias.unam.mx>
2020-02-25 11:32:33 -0600

    Tiene lo que vimos en la practica 1

--- Practica3Listas/src/ListaDL.java
+++ Practica3Listas/src/ListaDL.java
@@ -14,12 +14,39 @@ public class ListaDL<T> implements Listable<T>{
 
     @Override
     public void push(T elem) {
-
+        /**
+         * Agrega lo hara al final
+         */
+        Nodo e = new Nodo(elem);
+        if (elements == 0){
+            head = tail = e;
+        }else{
+            tail.next = e;
+            e.prev = tail;
+            tail = e;
+        }
+        elements++;
     }
 
     @Override
     public void pop(T elem) {
+        Nodo buqueda = search(elem, head);
+        if(buqueda != null){
+            if (elements == 1){
+                head = tail = null;
+            }else if (buqueda == head){
+                head = buqueda.next;
+                head.prev = null;
+            }else if (buqueda == tail){
+                tail = buqueda.prev;
+                tail.prev = null;
 
+            }else{
+                buqueda.prev.next = buqueda.next;
+                buqueda.next.prev = buqueda.prev;
+            }
+            elements--;
+        }
     }
 
     @Override
@@ -30,10 +57,34 @@ public class ListaDL<T> implements Listable<T>{
 
     public class Nodo{
         private T elemento;
-        private Nodo anterior;
-        private Nodo siguiente;
+        private Nodo prev;
+        private Nodo next;
+
+        /**
+         * Constructor por omision
+         * @param elemento
+         */
+        public Nodo(T elemento){
+            this.elemento = elemento;
+        }
     }
 
-
-
+    /**
+     * Siempre recorrer hacia el siguiente hasta encontrar el elementos
+     * @param element elemento a buscar
+     * @param pivote desde donde se empezara a buscar
+     * @return elementos encontrado
+     */
+    private Nodo search(T element, Nodo pivote){
+        while (pivote != null){
+            if (pivote.elemento.equals(element)){
+                return pivote;
+            }else {
+                pivote = pivote.next;
+            }
+        }
+        return null;
+    }
 }
+
+

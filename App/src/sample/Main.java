5c2ec8b7906cc1229e674bfe0d1d6c38f56efe85
arielmerin <arielmerino@ciencias.unam.mx>
2020-03-27 23:17:35 -0600

    Añadiendo los archivos de app

--- App/src/sample/Main.java
+++ App/src/sample/Main.java
@@ -1,23 +1,19 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        System.out.println(algoritmoEcuclides(30,42));
+    }
+    public static int algoritmoEcuclides(int a, int b){
+        if (a == 0){
+            return b;
+        }
+        return algoritmoEcuclides(  b%a, a);
     }
 }
+
